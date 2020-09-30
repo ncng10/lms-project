a74737aa2732dd90f4a07caaff8f02d1c4818e7a
nolan <ncongnolancong@gmail.com>
2020-09-30 15:54:35 -0700

    created routes for register and login. Also included bcrypt and jsonwebtoken

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,12 +1,59 @@
 const router = require("express").Router();
+const pool = require('../db');
+var bcrypt = require('bcrypt');
+const jwtGenerator = require('../utils/jwtGenerator');
 
 //register
-router.post('/', async (req, res) => {
+router.post('/register', async (req, res) => {
     try {
+        const { name, email, password } = req.body;
+        const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+            email
+        ]);
 
+        const saltRound = 10;
+        const salt = await bcrypt.genSalt(saltRound);
+        const bcryptPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await pool.query("INSERT INTO users (user_name, user_email, user_password) VALUES($1,$2,$3) RETURNING *", [
+            name, email, bcryptPassword
+        ]);
+
+        const token = jwtGenerator(newUser.rows[0].user_id);
+        if (user.rows.length !== 0) {
+            return res.status(401).send("User already exists")
+        }
+
+        res.json({ newUser, token });
     } catch (err) {
         console.log(err.message);
         res.status(500).send("server error");
+    }
+})
+
+//login
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [
+            email
+        ]);
+        if (user.rows.length === 0) {
+            return res.status(401).json('Password and/or email is incorrect does not exist')
+        }
+
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+        console.log(validPassword);
+        if (!validPassword) {
+            return res.status(401).json('Password and/or email is incorrect');
+        }
+        const token = jwtGenerator(user.rows[0].user_id);
+        res.json({ token })
+
+    } catch (err) {
+        console.log(err.message)
     }
 })
 
