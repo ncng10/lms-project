d6290113f8adb7eed1f2db88b942948821c82580
nolan <ncongnolancong@gmail.com>
2020-10-28 15:35:17 -0700

    .

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
 let secret_key = 'cf534gsgsdg2g' || process.env.JWT_SECRET
-module.exports = (req, res, next) => {
-    const jwtToken = req.header("token");
-    if (!jwtToken) {
-        return res.status(403).json("Not authorized 1");
-    }
+module.exports = async (req, res, next) => {
     try {
+        const jwtToken = req.header("token");
+        if (!jwtToken) {
+            return res.status(403).json("Not authorized");
+        }
         const payload = jwt.verify(jwtToken, secret_key);
         req.user = payload.user
-        next();
+
     } catch (err) {
         console.log(err.message)
-        return res.status(403).json("Not authorized 2");
+        return res.status(403).json("Not authorized");
     }
+    next();
 }
