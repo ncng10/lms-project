4869a71bf407d1bc8d4af79b075c2650f3b32ffc
nolan <ncongnolancong@gmail.com>
2020-10-28 15:26:25 -0700

    index

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client/build")))
 }
+app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+});
 // app.get("*", (req, res) => {
 //     res.sendFile(path.join(__dirname, "client/build/index.html"))
 // })
