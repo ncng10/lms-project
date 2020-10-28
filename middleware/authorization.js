c3194f03884d509b9a6a2b7ba462763c9d89a9d8
nolan <ncongnolancong@gmail.com>
2020-10-28 13:43:26 -0700

    adaaaa

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
-
+let secret_key = 'cf534gsgsdg2g' || process.env.JWT_SECRET
 module.exports = async (req, res, next) => {
     try {
         const jwtToken = req.header("token");
         if (!jwtToken) {
             return res.status(403).json("Not authorized");
         }
-        const payload = jwt.verify(jwtToken, process.env.JWT_SECRET);
+        const payload = jwt.verify(jwtToken, secret_key);
         req.user = payload.user
 
     } catch (err) {
