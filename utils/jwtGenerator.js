6a6b7fad99860473b500ca46c54f04c4979cf0fa
nolan <ncongnolancong@gmail.com>
2020-10-28 13:26:57 -0700

    jwt stuff

--- utils/jwtGenerator.js
+++ utils/jwtGenerator.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-
+let secret_key = process.env.JWT_SECRET || 'ncccc'
 function jwtGenerator(user_id) {
     const payload = {
         user: user_id
     }
-    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "2h" });
+    return jwt.sign(payload, process.env.secret_key, { expiresIn: "2h" });
 }
 
 module.exports = jwtGenerator;
