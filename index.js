660e1a3fd18c64b12bc98d5f7f83a91e3f81aa41
nolan <ncongnolancong@gmail.com>
2020-10-27 21:51:22 -0700

    fixed port issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require('./db');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const path = require("path");
 app.use(cors());
 app.use(express.json());
