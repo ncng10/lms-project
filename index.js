20f20c05841896319ebc0a091b4a8e8a5d1c3cbc
nolan <ncongnolancong@gmail.com>
2020-10-28 19:51:14 -0700

    m

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client/build/")))
-    app.get('*', (request, response) => {
-        response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
 }
+app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+});
 
 // app.get("*", (req, res) => {
 //     res.sendFile(path.join(__dirname, "client/build/index.html"))
