efaca3c9c9f7003edf8b219fca220f28f97e3582
nolan <ncongnolancong@gmail.com>
2020-10-28 19:48:45 -0700

    .

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.json());
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build/index.html")))
+    app.use(express.static(path.join(__dirname, "client/build/")))
     app.get('*', (request, response) => {
-        response.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+        response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
 }
 
