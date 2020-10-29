56c3002c9a19405680fb7ca4896450f33f029398
nolan <ncongnolancong@gmail.com>
2020-10-28 22:52:20 -0700

    a

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ const path = require("path");
 app.use(cors());
 app.use(express.json());
 
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build/")))
-    app.get('*', (request, response) => {
-        response.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-    });
-}
-
 // app.get("*", (req, res) => {
 //     res.sendFile(path.join(__dirname, "client/build/index.html"))
 // })
@@ -26,3 +18,10 @@ app.use("/course-page", require('./routes/coursepage'));
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
 });
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build/")))
+    app.get('*', (request, response) => {
+        response.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    });
+}
